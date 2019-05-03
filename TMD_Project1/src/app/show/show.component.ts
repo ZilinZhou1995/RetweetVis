59ca4085694e8f13c1ec6da87baaf5cf5649dee9
Jasoncool <jasonsu@JasondeMacBook-Pro.local>
2019-05-02 20:38:02 -0400

    some changes

--- TMD_Project1/src/app/show/show.component.ts
+++ TMD_Project1/src/app/show/show.component.ts
@@ -20,34 +20,52 @@ export class ShowComponent {
           getIndex = number => number - 1;
 
     /** constructing the nodes array */
-    for (let i = 1; i <= N; i++) {
+    // for (let i = 1; i <= N; i++) {
+    //   this.nodes.push(new Node(i));
+    // }
+
+    for (let i = 0; i < this.data.length; i++){
       this.nodes.push(new Node(i));
+      console.log(i);
     }
 
-    for (let i = 1; i <= N; i++) {
-      for (let m = 2; i * m <= N; m++) {
-        /** increasing connections toll on connecting nodes */
-        this.nodes[getIndex(i)].linkCount++;
-        this.nodes[getIndex(i * m)].linkCount++;
+    // for (let i = 1; i <= N; i++) {
+    //   for (let m = 2; i * m <= N; m++) {
+    //     /** increasing connections toll on connecting nodes */
+    //     this.nodes[getIndex(i)].linkCount++;
+    //     this.nodes[getIndex(i * m)].linkCount++;
 
-        /** connecting the nodes before starting the simulation */
-        this.links.push(new Link(i, i * m));
-      }
+    //     /** connecting the nodes before starting the simulation */
+    //     this.links.push(new Link(i, i * m));
+    //   }
+    // }
+
+    for (let i = 1; i < this.data.length; i++) {
+      this.nodes[i].linkCount++;
+      this.nodes[i-1].linkCount++;
+
+      this.links.push(new Link(i, i-1));
     }
+
   }
 
   ngOnInit() {
     let i = 4;
-    for(let node of this.nodes) {
-      if(node.id == '1' || node.id == '16' || node.id == '71' || node.id == '84' || node.id == '21' || node.id == '89') {
-        node.timestamp = d[i].created_at;
-        node.name = d[i].user.name;
-        i = Math.abs(i - 1) % 5;
-      } else {
-        node.timestamp = "Sat Apr 13 18:39:10 +0000 2019";
-        node.name = "Juan Guido";
-      }
-      
+    // for(let node of this.nodes) {
+    //   if(node.id == '1' || node.id == '16' || node.id == '71' || node.id == '84' || node.id == '21' || node.id == '89') {
+    //     node.timestamp = d[i].created_at;
+    //     node.name = d[i].user.name;
+    //     i = Math.abs(i - 1) % 5;
+    //   } else {
+    //     node.timestamp = "Sat Apr 13 18:39:10 +0000 2019";
+    //     node.name = "Juan Guido";
+    //   } 
+    // }
+
+    for (let node of this.nodes) {
+      node.timestamp = d[i].created_at;
+      node.name = d[i].user.name;
+      i = Math.abs(i - 1) % 5;
     }
     
   }
